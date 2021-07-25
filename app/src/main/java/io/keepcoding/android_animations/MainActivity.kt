f7577cbd4972c1434e36a9627250bb6e8792f106
Tim Acosta <timacosta06@gmail.com>
2021-07-25 19:02:48 +0100

    animator works, but crappy

--- app/src/main/java/io/keepcoding/android_animations/MainActivity.kt
+++ app/src/main/java/io/keepcoding/android_animations/MainActivity.kt
@@ -1,16 +1,30 @@
 package io.keepcoding.android_animations
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.keepcoding.android_animations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var animator: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        ActivityMainBinding.inflate(layoutInflater).also {
+        val bining = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
 
+        val playStopAnimationButton = bining.playStopButton.apply {
+            setImageDrawable(getDrawable(R.drawable.play_stop));
+            animator = drawable as AnimationDrawable
+        }
+
+        playStopAnimationButton.setOnClickListener{
+            animator.start()
+        }
+
     }
+
 }
